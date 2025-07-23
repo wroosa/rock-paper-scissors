4225bd318268354687901f821192f02a595fc3d9
Walter R <wroosa111@gmail.com>
2025-07-23 12:14:43 -0400

    Add basic page and script to get computer and human choice
    Add simple HTML page and a js file containing the functionality to generate a computer choice and prompt the user for a choice. Add a section to test randomness by generating computer choices and displaying the distribution of results.

---
+++ main.js
@@ -0,0 +1,59 @@
+// Prompt the user for a string input of 'rock', 'paper', or 'scissors'
+function getHumanChoice() {
+  // Assume the user will always input a valid string
+  return prompt("Please select 'rock', 'paper', or 'scissors':");
+}
+
+// Generate a string between 'rock', 'paper', and 'scissors' at random
+function getComputerChoice() {
+  let randomNum = getRandomInt(1, 3);
+  if (randomNum === 1) {
+    return "scissors";
+  } else if (randomNum === 2) {
+    return "rock";
+  } else if (randomNum === 3) {
+    return "paper";
+  }
+}
+
+// Generate a random integer between a min and a max inclusive
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+
+  /* 
+  Get the size of the range between max and min (max - min + 1) and multiply it by a random fraction (Math.random()) which gives you the point between the Max and Min range that was generated. Slide that point up my the min (+ min) and then round down to get the random integer.
+  */
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// Randomness Testing //
+
+// Initialize result counts
+let rock = 0;
+let paper = 0;
+let scissors = 0;
+
+// Loop through a specific number of tests and store the results
+const NUMBER_OF_TESTS = 1000;
+
+for (let i = 1; i < NUMBER_OF_TESTS; i++) {
+  let result = getComputerChoice();
+  if (result === "scissors") {
+    scissors++;
+  } else if (result === "rock") {
+    rock++;
+  } else if (result === "paper") {
+    paper++;
+  }
+}
+
+// Calculate the percentage for each type of result
+rockPercent = Math.round((rock / NUMBER_OF_TESTS) * 100);
+paperPercent = Math.round((paper / NUMBER_OF_TESTS) * 100);
+scissorsPercent = Math.round((scissors / NUMBER_OF_TESTS) * 100);
+
+// Display the percentages for each type of result
+alert(
+  `rock:${rockPercent}% paper:${paperPercent}% scissors:${scissorsPercent}%`
+);
